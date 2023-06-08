8c4b9c386ff2168c0fe2386ab24ea41832bad5f7
maksym.bohoniuk <maksym.bohoniuk@onwelo.com>
2023-06-08 15:25:24 +0200

    [Task f4] refactor

--- lib/task_f4/main.dart
+++ lib/task_f4/main.dart
@@ -7,6 +7,63 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static List<Question> questions = [
+    Question(
+        category: QuestionCategory.Cars,
+        text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
+        imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Space,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Football,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Cars,
+        text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
+        imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Space,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Football,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Cars,
+        text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
+        imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Space,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
+        color: randomColor()
+    ),
+    Question(
+        category: QuestionCategory.Football,
+        text: "bla bla bla bla bla bla ?",
+        imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
+        color: randomColor()
+    )
+  ];
+  static const SizedBox divider = SizedBox(width: 20,);
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -28,88 +85,7 @@ class MyApp extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 // physics: const BouncingScrollPhysics(),
                 child: Row(
-                  children: [
-                    QuestionWidget(
-                        question: Question(
-                          category: "Cars",
-                          text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
-                          imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
-                          color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Space",
-                            text: "bla bla bla bla bla bla ?",
-                            imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
-                            color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Football",
-                            text: "bla bla bla bla bla bla ?",
-                            imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
-                            color: randomColor()
-                        ),
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Cars",
-                            text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
-                            imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
-                            color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Space",
-                            text: "bla bla bla bla bla bla ?",
-                            imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
-                            color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                      question: Question(
-                          category: "Football",
-                          text: "bla bla bla bla bla bla ?",
-                          imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
-                          color: randomColor()
-                      ),
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Cars",
-                            text: "bla bla bla bla bla bla bla bla bla bla bla bla bla bla v bla bla bla bla bla bla bla bla bla bla bla bla bla bla v?",
-                            imageUrl: "https://super-cars.pl/wp-content/uploads/2022/02/bmw-1620x1080.jpg",
-                            color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                        question: Question(
-                            category: "Space",
-                            text: "bla bla bla bla bla bla ?",
-                            imageUrl: "https://as2.ftcdn.net/v2/jpg/02/79/03/07/1000_F_279030771_pMo4u606vF6mscufYoroxD2r1Wt5zmPe.jpg",
-                            color: randomColor()
-                        )
-                    ),
-                    const SizedBox(width: 20,),
-                    QuestionWidget(
-                      question: Question(
-                          category: "Football",
-                          text: "bla bla bla bla bla bla ?",
-                          imageUrl: "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
-                          color: randomColor()
-                      ),
-                    ),
-                  ],
+                  children: getChildren(),
                 ),
               ),
             ),
@@ -118,11 +94,21 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  Color randomColor(){
+  static Color randomColor(){
     int randomIndex = Random().nextInt(Colors.primaries.length);
     Color randomColor = Colors.primaries.elementAt(randomIndex);
     debugPrint("Random color is ${randomColor.toString()}");
     return randomColor;
+  }
+
+  List<Widget> getChildren(){
+    List<Widget> children = [];
+    for (Question element in questions) {
+      children.add(QuestionWidget(question: element));
+      children.add(divider);
+    }
+    children.removeLast();
+    return children;
   }
 }
 
@@ -150,7 +136,7 @@ class QuestionWidget extends StatelessWidget {
               color: _question.color,
               borderRadius: const BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15))
             ),
-            child: Center(child: Text(_question.category, style: const TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),)),
+            child: Center(child: Text(_question.category.name, style: const TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),)),
           ),
           Expanded(
             child: Container(
@@ -171,11 +157,15 @@ class QuestionWidget extends StatelessWidget {
 }
 
 class Question{
-  late final String category;
+  late final QuestionCategory category;
   late final String text;
   late final String imageUrl;
   late final Color color;
 
   Question({required this.category,required this.text, required this.imageUrl, required this.color});
 
+}
+
+enum QuestionCategory{
+  Cars, Football, Space
 }
